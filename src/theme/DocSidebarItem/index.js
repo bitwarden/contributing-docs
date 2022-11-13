360c61373bd54d5ba46f25db3d40064c2499f4ee
Hinton <hinton@users.noreply.github.com>
2022-11-13 20:20:05 +0100

    Cleanup

--- src/theme/DocSidebarItem/index.js
+++ src/theme/DocSidebarItem/index.js
@@ -1,16 +1,13 @@
-import React from 'react';
-import DocSidebarItem from '@theme-original/DocSidebarItem';
+import React from "react";
+import DocSidebarItem from "@theme-original/DocSidebarItem";
 
 import { useDevMode } from "@site/src/contexts/devMode";
 
 export default function DocSidebarItemWrapper(props) {
-
   const { devMode } = useDevMode();
 
-  if (props.item?.customProps?.access) {
-    if (props.item.customProps.access != devMode) {
-      return null;
-    }
+  if (props.item?.customProps?.access && props.item.customProps.access != devMode) {
+    return null;
   }
 
   return (
