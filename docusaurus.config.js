69c0dc1e4c2e39fc71d5e368dcc7adb764a7e98d
Andreas Coroiu <andreas.coroiu@gmail.com>
2024-02-01 16:16:51 +0100

    add glossary

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -2,6 +2,7 @@
 // Note: type annotations allow type checking and IDEs autocompletion
 
 import { themes } from "prism-react-renderer";
+import remarkDefList from "remark-deflist";
 
 async function createConfig() {
   const { remarkKroki } = await import("remark-kroki");
@@ -36,7 +37,7 @@ async function createConfig() {
             // Please change this to your repo.
             // Remove this to remove the "edit this page" links.
             editUrl: "https://github.com/bitwarden/contributing-docs/tree/main/",
-            remarkPlugins: [[remarkKroki, { server: "https://kroki.io/" }]],
+            remarkPlugins: [remarkDefList, [remarkKroki, { server: "https://kroki.io/" }]],
           },
           theme: {
             customCss: require.resolve("./src/css/custom.css"),
